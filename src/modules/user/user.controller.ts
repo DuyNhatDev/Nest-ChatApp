20d5db4cef82a7f06075bd11a0e4e9db07df512b
DuyNhatDev <duynhat1507@gmail.com>
2026-02-27 01:59:51 +0700

    Lấy thông tin cá nhân

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get, Req } from '@nestjs/common'
+import { UserService } from './user.service'
+import type { Request as ExpressRequest } from 'express'
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  @Get('/me')
+  getMe(@Req() req: ExpressRequest) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) return null
+    const accessToken = authHeader.split(' ')[1]
+    return this.userService.getMe(accessToken)
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { UserRepository } from '@/modules/user/user.repo'
+import { TokenService } from '@/shared/services/token.service'
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly tokenService: TokenService,
+  ) {}
+  async getMe(accessToken: string) {
+    const { userId } = await this.tokenService.verifyAccessToken(accessToken)
+    return this.userRepository.findUnique({ _id: userId })
+  }
+}
